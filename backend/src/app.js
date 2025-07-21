d3db8d943f8d26fb3624b7c8aa5cbdff5602e0a1
Tanmay Kanase <157377290+Tanmay-kanase@users.noreply.github.com>
2025-07-21 17:09:14 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,12 @@ const userRoutes = require("./routes/userRoutes");
 const profileRoutes = require("./routes/creatorProfileRoutes");
 const campaignRoutes = require("./routes/campaignRoutes");
 const app = express();
-
+app.use(
+  cors({
+    origin: "https://cbcp.onrender.com", // ✅ Allow only your frontend
+    credentials: true, // ✅ If you're using cookies/auth
+  })
+);
 
 app.use(express.json());
 
